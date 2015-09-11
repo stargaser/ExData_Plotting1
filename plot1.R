531051d629bc9bbcf4d17de1f4d574d99c19ee83
David Shupe <dave.shupe@gmail.com>
2015-09-11 11:08:08 -0700

    script and plot for plot1

---
+++ plot1.R
@@ -0,0 +1,21 @@
+
+# Read in the data.
+# Here we don't try to use the Date class.
+elec <- read.csv("../household_power_consumption.txt", na.strings="?",
+                  sep=";", header=TRUE, 
+                  colClasses=c("character","character", "numeric",
+                               "numeric","numeric","numeric","numeric",
+                               "numeric","numeric"))
+
+# Keeping the date as a string allows us to select the days of interest.
+subdf <- elec[grep("^[12]/2/2007", elec$Date),]
+
+# open a PNG device
+png(filename="plot1.png")
+
+# Make the histogram in bins of 0.5.
+hist(subdf$Global_active_power, breaks=seq(0,7.5,0.5), 
+     col="red", main="Global Active Power",
+     ylab="Frequency", xlab="Global Active Power (kilowatts)")
+
+dev.off()
